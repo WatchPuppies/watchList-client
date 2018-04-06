4ffd317ab1954a6d5ed764c3529ca1d296a07846
yohanessahrul <yohanessahrul92@gmail.com>
2018-04-06 13:05:01 +0700

    search series

--- script/searchSeries.js
+++ script/searchSeries.js
@@ -1,28 +1,44 @@
 $('#search').click(function() {
     let query = $('#query').val()
 
-    $('#result').load('category.html #result')
 
+    $('#result').load('search_series.html #result')
+
+    console.log('query', query)
     $.ajax({
-        url: `localhost:3000/show/search/series/${query}`,
+        url: `http://localhost:3000/show/search/series/${query}`,
         method: "GET",
         dataType: "JSON",
         success: function(result) {
+            console.log('resule', result)
             result.forEach(series => {
-                $('#series').append(`
+                // console.log(series.show.image.medium)
+
+                let image;
+                if(series.show.image == null){
+                    image = 'no images'
+                } else {
+                    image = `<img src="${series.show.image.medium}"/>`
+                }
+                $('#result').append(`
                 <tr>
-                  <td>${series.name}</td>
-                  <td>series</td>
-                  <td>${series.type}</td>
-                  <td>${series.rating}</td>
-                  <td>${series.runtime}</td>
-                  <td>${series.summary}</td>
+                  <td>${image}</td>
+                  <td>${series.show.name}</td>
+                  <td>Series</td>
+                  <td style="width:100px;">${series.show.genres}</td>
+                  <td>${series.show.rating.average}</td>
+                  <td>${series.show.runtime} min</td>
+                  <td>${series.show.summary}</td>
                   <td>
                     <a class="btn btn-sm btn-primary" href="#">Add Playlist</a>
                   </td>
                 </tr>
                 `)
             });
+        },
+        error: function(err) {
+          console.log('error', err.message);
+          
         }
     })
 
